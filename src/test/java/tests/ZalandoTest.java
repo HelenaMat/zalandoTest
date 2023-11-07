2db9001589dba456c01658025a88bf914984d9d1
HelenaMat <helena.matikainen@hotmail.se>
2023-11-07 11:38:06 +0100

    ZalandoTest

---
+++ src/test/java/tests/ZalandoTest.java
@@ -0,0 +1,42 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Playwright;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+public class TestBase {
+    // Deklarera Playwright-objekt
+    private static Playwright playwright;
+    // Deklarera Browser-objekt
+    protected static Browser browser;
+    // Deklarera BrowserContext-objekt
+    protected BrowserContext context;
+
+    @BeforeClass
+    public static void setUp() {
+        // Initialisera Playwright och skapa en webbläsarinstans
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @Before
+    public void createNewContext() {
+        // Skapa en ny kontext (webbläsarsession) för varje testfall
+        context = browser.newContext();
+    }
+
+    @After
+    public void closeContext() {
+        // Stäng den aktuella kontexten efter varje testfall
+        context.close();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        // Stäng webbläsaren och släpp Playwright-resurserna när alla test är klara
+        browser.close();
+        playwright.close();
+    }
+}
